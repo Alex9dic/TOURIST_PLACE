c62240d5568ddf4f0b314543bb02b697060895a1
Alex9dic <alexidanielramirez@gmail.com>
2022-11-23 17:13:51 -0600

    Vista

---
+++ src/main/java/com/mycompany/entity/imagesView.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author labso11
+ */
+@Named
+@RequestScoped
+public class imagesView {
+
+    private List<String> images;
+
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 12; i++) {
+            images.add("nature" + i + ".jpg");
+        }
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+}
