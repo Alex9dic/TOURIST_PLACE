2cd5523c4ecbfa441625429ec183f91a2df0f7d0
Ramixramz <ramixramirez18@gmail.com>
2022-11-23 17:44:23 -0600

    Modulo Controller Reservacion

--- src/main/java/com/mycompany/model/ReservaModelImpl.java
+++ src/main/java/com/mycompany/model/ReservaModelImpl.java
@@ -6,31 +6,77 @@ package com.mycompany.model;
 
 import com.mycompany.entity.Reservaciones;
 import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 /**
  *
  * @author labso11
  */
 public class ReservaModelImpl implements IReservaModel{
+    
+    private SessionFactory sf;
+    private Session sesion;
+    
 
     @Override
     public void insertarRegistro(Reservaciones reserva) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+      try {
+            sf = new Configuration().configure().buildSessionFactory();
+            sesion = sf.openSession();
+            sesion.beginTransaction();
+            sesion.save(reserva);
+            sesion.getTransaction().commit();
+            sesion.close();
+        } catch (HibernateException e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 
     @Override
     public List<Reservaciones> obtenerRegistro() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         List<Reservaciones> listaRol = null;
+        try {
+            sf = new Configuration().configure().buildSessionFactory();
+            sesion = sf.openSession();
+            listaRol = sesion.createCriteria(Reservaciones.class).list();
+            sesion.close();
+        } catch (HibernateException e) {
+            System.out.println("Error" + e.getMessage());
+        }
+        return listaRol;
     }
 
     @Override
     public void actualizarRegistro(Reservaciones reserva) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            sf = new Configuration().configure().buildSessionFactory();
+            sesion = sf.openSession();
+            sesion.beginTransaction();
+            sesion.update(reserva);
+            sesion.getTransaction().commit();
+            sesion.close();
+            System.out.println("Actualizado");
+        } catch (HibernateException e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
 
     @Override
     public void eliminarRegistro(Reservaciones reserva) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+            sf = new Configuration().configure().buildSessionFactory();
+            sesion = sf.openSession();
+            sesion.beginTransaction();
+            sesion.delete(reserva);
+            sesion.getTransaction().commit();
+            sesion.close();
+            System.out.println("Eliminado");
+        } catch (HibernateException e) {
+            System.out.println("Error" + e.getMessage());
+        }
     }
     
 }
